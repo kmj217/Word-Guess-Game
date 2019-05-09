4f4a1b8a3c9e157e177cb52bdccc84fd0cb21a28
kmj217 <kmj217@hotmail.com>
2019-05-09 19:50:08 -0400

    randomly selecting games works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,6 +10,32 @@ var game = games[Math.floor(Math.random()* games.length)];
 //use _'s to show length of game
 
 var answerArray =[];
-for (var i = 0; i < game.length; i++) {
-    answerArray[i] = "_";
+
+//variable for remaining letters
+
+var remainingLetters = game.length;
+
+//variable to start and stop game
+
+var gameStart = false;
+
+//make a function to reset the game 
+
+function reset() {
+    gameStart = true;
+    game = games[Math.floor(Math.random()* games.length)];
+    remainingLetters = game.length;
 }
+
+console.log(game)
+//randomly select word
+
+//have blanks show number of letters in word
+    //count number of letters in selected word
+    //create _'s for number of letters with for loop
+    //game will start when the player pressed enter
+    //for loop through word 
+
+
+
+
