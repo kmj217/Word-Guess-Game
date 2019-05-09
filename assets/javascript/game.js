b5cdf8afde575746448b33c6fc2410ee36c103f6
kmj217 <kmj217@hotmail.com>
2019-05-09 18:51:05 -0400

    started inital variables and arrays

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,15 @@
-//Variables
+//array of games
 
-var games = ["Mortal Kombat", "Super Mario", "Doom", "Sonic", "Quake", "Super Metroid", "Crono Trigger"  ]
+var games = ["Mortal Kombat", "Super Mario", "Doom", "Sonic", 
+            "Quake", "Super Metroid", "Crono Trigger"];
 
+//random game from array
 
+var game = games[Math.floor(Math.random()* games.length)];
 
+//use _'s to show length of game
+
+var answerArray =[];
+for (var i = 0; i < game.length; i++) {
+    answerArray[i] = "_";
+}
